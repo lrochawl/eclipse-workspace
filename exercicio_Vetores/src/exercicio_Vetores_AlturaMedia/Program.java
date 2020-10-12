5f5f3b054a7761f5cd712b2fc6ce4209061ea0f7
Lucas Rocha <wltopos@gmail.com>
2020-10-12 16:48:57 -0300

    Exercicio - Vetor

---
+++ exercicio_Vetores/src/exercicio_Vetores_AlturaMedia/Program.java
@@ -0,0 +1,40 @@
+package exercicio_Vetores_AlturaMedia;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantas pessoas serão analisadas: ");
+		int n = sc.nextInt();
+		
+		double[] vect = new double [n];
+		
+		for(int i=0; i<n; i++) {
+			
+			System.out.printf("Qual a altura da %d pessoa: ", i + 1);
+			vect[i] = sc.nextDouble();
+			
+			
+		}
+		
+		double soma = 0.0;
+		for(int i=0; i<n; i++) {
+		soma += vect[i];
+				
+		}	
+		
+		double alturaMedia = soma / n;
+		
+		System.out.printf("A altura média é: %.2f", alturaMedia);
+		sc.close();
+
+	}
+
+}
